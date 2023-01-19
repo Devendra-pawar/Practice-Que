9e1f6b00f2711eb00ecbff27f6d46f4faefed189
Devendra pawar <iamdevendra06@gmail.com>
2023-01-20 00:08:11 +0530

    19/01

--- PrepInsta/165+_codes.py
+++ PrepInsta/165+_codes.py
@@ -1,3 +1,5 @@
+# Getting Started-----------------------------------------------------------
+
 #Question no. 1 
 
 # num = int(input("Enter any no.:"))
@@ -34,8 +36,105 @@
 
 #Question no. 5
 
-low = int(input("Enter the low of the number:"))
-high= int(input("Enter the high of the number:"))
+# low = int(input("Enter the low of the number:"))
+# high= int(input("Enter the high of the number:"))
+# sum = 0 
+# for i in range(low,high+1):
+#     sum += i
+# print(sum)
 
-for i in range():
+
+#Question no. 6
+
+# num1 = int(input("Enter any number:"))
+# num2 = int(input("Enter any number:"))
+
+# if num1 > num2:
+#     print("Greatest number is :", num1 )
+# elif num1 == num2 :
+#     print("Both are equal")
+# else:
+#     print("Greatest number is:",num2)
+
+
+#Question no. 7
+
+# num1 = int(input("Enter first number:"))
+# num2 = int(input("Enter second number:"))
+# num3 = int(input("Enter third number:"))
+
+# if num1 >num2 :
+#     if num1 >num3 :
+#         print("Greatest number is:",num1)
+#     else:
+#         print("Greatest number is:",num3)
+# else:
+#     if num2 >num3 :
+#         print("Greatest number is:",num2)
+#     else :
+#         print("Greatest number is:",num3)
+        
+
+#Question no. 8
+# print("Checking a year is a leap year or not-")
+# year = int(input("Enter any year:"))
+
+# if (year%4 == 0 and year%100 !=0 ) or year %400== 0 :
+#     print("It's a leap Year")
+# else:
+#     print("Not a leap year")
+
+
+#Question no. 9
+
+# num = int(input("Enter any number:"))
+
+# if num > 1 :
+#     count = 0
+#     for i in range(2,num):
+#         if num%i ==0:
+#            count += 1
+#     if count ==0 :
+#         print("It is a prime number")
+#     else:
+#         print("It is not a prime number")
+# else :
+#     print("Not prime")
+
+
+#Question no. 10
+
+# low = int(input("Enter the low of the number:"))
+# high= int(input("Enter the high of the number:"))
+
+# for i in range(low, high+1):
+
+#     count = 0
+#     for j in range(2,i):       
+#         if i % j == 0:
+#             count+=1
+#     if count == 0:
+#         print(i,end=" ")
+
+
+
+
+#Working with Numbers--------------------------------------
+
+
+
+#Codes for Recursion---------------------------------------
+
+
+
+
+#Important Codes related to Arrays-------------------------
+
+
+
+
+#Operations on Strings-------------------------------------
+
+
+
 
