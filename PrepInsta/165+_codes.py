67271ca3f7a0d2767151e28d5269f1394cb2dff3
Devendra pawar <iamdevendra06@gmail.com>
2023-01-20 23:58:22 +0530

    20/01/23

--- PrepInsta/165+_codes.py
+++ PrepInsta/165+_codes.py
@@ -117,23 +117,214 @@
 #         print(i,end=" ")
 
 
+#Question no. 11
+
+# num = int(input("Enter any number :"))
+# sum = 0 
+# while num > 0 :
+#     single = num% 10
+#     num = num//10
+#     sum += single
+# print(sum)
+
+
+#Question no. 12
+# num = int(input("Enter any number :"))
+# rev = 0 
+# while num > 0 :
+#     single = num% 10
+#     num = num//10
+#     rev = rev*10 + single
+# print(rev)
+
+
+#Question no. 13
+
+
+
+
+
+
+
+
 
 
 #Working with Numbers--------------------------------------
 
+#Question no . 1 
+
+# num1 = 36
+# num2 = 60
+# hcf = 1
+
+# for i in range(1,min(num1,num2)):
+#     if num1%i==0 and num2%i==0:
+#         hcf = i
+# print("Highest common factor is ",hcf)
+
+
+#Question no. 2 
+
+# num1 = 12
+# num2 = 14
+
+# for i in range(max(num1,num2),(num1*num2)+1):
+#     if i % num1 ==0 and i % num2 ==0:
+#         lcm = i
+#         break
+# print("Least common multiplier is ",lcm)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #Codes for Recursion---------------------------------------
+
+#Question no. 1
+
+# def fun(a,b):
+#     if b ==0:
+#         return 1 
+#     else:
+#         return a*fun(a,b-1)
+# base = 2
+# power= 3
+# print("answer is",fun(base,power))
+
+
+#Question no. 2
+
+# def prime(n,i):
+#     if n==i :
+#         return True
+#     elif n %i ==0:
+#         return False
+    
+#     else:
+#         return prime(n,i+1) 
+
+# n = 971
+# i = 2
+# if prime(n,i):
+#     print("It is a prime no.")
+# else :
+#     print("It is not a prime no.")
+
+
+#Question no. 3
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 #Important Codes related to Arrays-------------------------
 
+# QUESTION NO. 1
+
+# a = [10, 89, 9, 56, 4, 80, 8]
+# print(max(a))
+
+#or
+
+# a = [10, 89, 9, 56, 4, 80, 8]
+# a.sort()
+# print(a[-1])
+
+#or 
+
+# a = [10, 89, 9, 56, 4, 80, 8]
+# max = a[0]
+# for i in range(len(a)):
+#     if a[i]>max:
+#         max = a[i]
+# print(max)
+
+#Question 2 
+
+# a = [10, 89, 9, 56, 4, 80, 8]
+# min = a[0]
+# for i in range(len(a)):
+#     if a[i]< min:
+#         min = a[i]
+# print(min)
+
+#Question 3
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 #Operations on Strings-------------------------------------
+
+# Question no. 1
+
+# c = input("Enter a character:")
+
+# if c == "a" or c == "e" or c == "i" or c == "o" or c == "u" or c == "A" or c == "E" or c == "I" or c == "O" or c == "U":
+#     print("It is a vowel")
+# else:
+#     print("It is a consonant")
+
+# Question no. 2
+
+# ch = input("Enter any character:")
+
+# if "a"<= ch <= "z" or "A"<= ch <= "Z":
+#     print("It is a alphabet ")
+# else:
+#     print("It is not a alphabet")
+
+
+#Question no. 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
